76d9e8ea8084a4198aa3f7b7fb02f316c8fccf4f
Anton Rusak <4231915+arusak@users.noreply.github.com>
2020-07-15 15:07:26 +0300

    Updates

---
+++ precache-manifest.d6bc08915c262da48f948e0a46359d9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80bb1a51002cdc0fe67ed0aac7244bcc",
+    "url": "/vdhr/index.html"
+  },
+  {
+    "revision": "ff60866b80751fb9ebb2",
+    "url": "/vdhr/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "59c7e93f12df9b9d2ac3",
+    "url": "/vdhr/static/js/2.c7a74e89.chunk.js"
+  },
+  {
+    "revision": "e12ed09be89d98e3b07b38f179289140",
+    "url": "/vdhr/static/js/2.c7a74e89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff60866b80751fb9ebb2",
+    "url": "/vdhr/static/js/main.6ad5eb2d.chunk.js"
+  },
+  {
+    "revision": "cd5730284af8cd3fbd11",
+    "url": "/vdhr/static/js/runtime-main.d6a5f21a.js"
+  }
+]);
