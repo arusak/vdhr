6943167a88ed3923bb65afb243e2c35ff1b6be8f
Anton Rusak <4231915+arusak@users.noreply.github.com>
2021-09-07 17:16:22 +0300

    Bugfix: removed end-of-year spike in leap years

--- src/components/chart/chart.container.tsx
+++ src/components/chart/chart.container.tsx
@@ -3,7 +3,7 @@ import AutoSizer from 'react-virtualized-auto-sizer';
 import { ChartComponent, LevelsByDate } from './chart.component';
 import { StateContext } from '../../contexts/state/state.context';
 import { DataServiceContext, Level } from '../../services';
-import { formatDate } from '../../utils/date.utils';
+import { formatDate, diffDates, parseDate } from '../../utils/date.utils';
 
 export const Chart = () => {
     const service = useContext(DataServiceContext);
@@ -18,17 +18,20 @@ export const Chart = () => {
             .finally(() => setLoading(false));
     }, [service]);
 
-    const chartData = levels.reduce<LevelsByDate[]>((res, observation) => {
-        const dateStr = formatDate(observation.date, 'dd.MM');
-        const yearStr = formatDate(observation.date, 'yyyy');
-        let levelsByDate = res.find((byDate) => byDate.date === dateStr);
-        if (!levelsByDate) {
-            levelsByDate = { date: dateStr };
-            res.push(levelsByDate);
-        }
-        levelsByDate[yearStr] = observation.level;
-        return res;
-    }, []);
+    const chartData = levels
+        .reduce<LevelsByDate[]>((res, observation) => {
+            const dateStr = formatDate(observation.date, 'dd.MM');
+            const yearStr = formatDate(observation.date, 'yyyy');
+            let levelsByDate = res.find((byDate) => byDate.date === dateStr);
+            if (!levelsByDate) {
+                levelsByDate = { date: dateStr };
+                res.push(levelsByDate);
+            }
+            levelsByDate[yearStr] = observation.level;
+            return res;
+        }, [])
+        // because we must have consistent x-axis for all years
+        .filter((v) => v.date !== '29.02');
 
     return isLoading ? (
         <div>Loading...</div>
